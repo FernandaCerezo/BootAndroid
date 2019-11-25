a213b26cd65439064596abea54bc94fc67d01ea6
FernandaCerezo <fernandacerezog@gmail.com>
2019-11-25 11:18:27 -0800

    Orientation

--- app/src/main/java/com/bootcamp/login/UserProfile.java
+++ app/src/main/java/com/bootcamp/login/UserProfile.java
@@ -5,29 +5,20 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.bootcamp.login.Users.Users;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserProfile extends AppCompatActivity {
     TextView mStartDate, mAccount, mEndDate, mName, mRol, mTechnology, mEspTechnology, mGrade;
     String mTitle, UserName, Rol, Technology, EspTechnology, Id;
     String Accounts, StartDate, EndDate, Grade;
-
-    private FirebaseDatabase mFirebasedatabase;
     DatabaseReference databaseReference;
 
 
