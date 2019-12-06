9fc50161487e71f4b90bb7f3851ddc1197f80590
Soulball <navarrete.de16@tectijuana.edu.mx>
2019-12-05 22:38:16 -0800

    2/2 Percent

--- app/src/main/java/com/bootcamp/login/ui/home/HomeFragment.java
+++ app/src/main/java/com/bootcamp/login/ui/home/HomeFragment.java
@@ -11,24 +11,29 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bootcamp.login.Accounts.Accounts;
 import com.bootcamp.login.Home;
 import com.bootcamp.login.MainActivity;
 import com.bootcamp.login.R;
-import com.bootcamp.login.Users.Users;
 import com.bootcamp.login.Utils;
 import com.bootcamp.login.firebase.FirebaseConnection;
 import com.bootcamp.login.firebase.interfaces.CallbackUsersDelegate;
 import com.bootcamp.login.firebase.models.User;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    //Variables locales
     private RecyclerView mRecycleTech;
     private RecyclerView mRecycleSrc;
+    private ArrayList<String> Technologies;
+    private ArrayList<Integer> Tech_Count;
 
     @Nullable
     @Override
@@ -40,6 +45,13 @@ public class HomeFragment extends Fragment {
         String [] Name=new String[]{"FontEnd","BackEnd","Móvil"};
         getUsers("Movil");
         mRecycleTech.setAdapter(new HorizontalAdapter(Name,null));
+
+        //Porcentajes de tecnologias
+        Technologies = new ArrayList<>();
+        Tech_Count = new ArrayList<>();
+        mRecycleSrc.setLayoutManager(new LinearLayoutManager(getContext()));
+        GetPercents();
+
         return root;
 
     }
@@ -63,6 +75,50 @@ public class HomeFragment extends Fragment {
             public void onCancelled(DatabaseError databaseError) {
                 Utils.Debug(MainActivity.class, databaseError.getMessage());
             }
+        });
+    }
+
+    private void GetPercents()
+    {
+        //Referenciar el hijo "usuarios"
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("user");
+        databaseReference.orderByChild("technology").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                //Limpieza
+                Technologies.clear();
+                Tech_Count.clear();
+                String tech;
+                int index;
+                int total = 0;
+
+                //Por cada objeto
+                for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                    tech = dataSnapshot1.child("technology").getValue().toString();
+
+                    if(tech.equals("")){ }
+                    else
+                    {
+                        if (Technologies.contains(tech))
+                        {
+                            index = Technologies.indexOf(tech);
+                            Tech_Count.set(index, Tech_Count.get(index) + 1);
+                        }
+                        else
+                        {
+                            Technologies.add(tech);
+                            Tech_Count.add(1);
+                        }
+                        total += 1;
+                    }
+                }
+                //Mandar listas al adapter
+                AdapterHome adapter = new AdapterHome(Technologies,Tech_Count,total);
+                mRecycleSrc.setAdapter(adapter);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
         });
     }
 }
